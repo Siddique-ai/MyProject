db2eb0baf90edb94f55ea90d3ba90c2e9b73a655
admin <admin@DESKTOP-RG3Q5CD>
2021-08-30 13:34:40 +0530

    1st commit for this project file

---
+++ trial2/src/trial2/Demo.java
@@ -0,0 +1,17 @@
+package trial2;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import oracle.jdbc.driver.OracleDriver;
+
+public class Demo {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		 
+			System.out.println("Hello world");
+		 
+	}
+
+}
